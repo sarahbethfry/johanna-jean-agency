2a3e81fdea5d84896c405cc1899e304d2a767104
Sarah Fry <sfry06@yahoo.com>
2021-01-12 10:24:36 -0800

    setting up files

---
+++ src/SuperChill/SuperChill.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./SuperChill.module.css";
+
+const SuperChill = () => {
+  return (
+    <div className="SuperChill">
+      <h1>Johanna Jean</h1>
+      <br />
+      <h3> Agency </h3>
+      <hr />
+    </div>
+  );
+};
+
+export default SuperChill;
