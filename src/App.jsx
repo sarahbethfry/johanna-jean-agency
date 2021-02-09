f33d0aae17eb52af1f473709cc9c38aa724c5008
Sarah Fry <sfry06@yahoo.com>
2021-02-09 13:04:12 -0800

    Revamping coming soon page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import classes from "./App.module.css";
 import ComingSoonBackground from "./ComingSoon/ComingSoonBackground.jsx";
-import GentleWave from "./GentleWave/GentleWave.jsx";
 
 function App() {
   return (
     <div className={classes.App}>
       <ComingSoonBackground />
-      <GentleWave />
     </div>
   );
 }
