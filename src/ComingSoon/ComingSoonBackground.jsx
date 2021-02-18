4af71d9e0e3d9bf49669bf24835752c819b35542
Sarah Fry <sfry06@yahoo.com>
2021-02-18 13:50:50 -0800

    Completely changed look of coming soon page

--- src/ComingSoon/ComingSoonBackground.jsx
+++ src/ComingSoon/ComingSoonBackground.jsx
@@ -1,13 +1,27 @@
 import React from "react";
-import ComingSoonTextOverlay from "./ComingSoonTextOverlay";
 import classes from "./ComingSoon.module.css";
 
 const ComingSoonBackground = () => {
   return (
-    <div className={classes.Background}>
-      <h1 className={classes.JohannaJean}>Johanna Jean</h1>
-      <h3 className={classes.Agency}>Agency</h3> <hr />
-      <ComingSoonTextOverlay />
+    <div className={classes.ComingSoon}>
+      COMING SOON
+      <ul>
+        <li className={classes.list1}>
+          <a
+            href="https://www.instagram.com/johannajeanagency/?hl=en"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={classes.instagram}
+          >
+            @johannajeanagency
+          </a>
+        </li>
+        <li className={classes.list2}>
+          <a href="mailto:johanna@johannajeanagency.com<">
+            johanna@johannajeanagency.com
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
